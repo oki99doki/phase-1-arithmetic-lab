de1df38c56cb0dff8a81bd921e8f07d6e6134d3b
oki99doki <s.klinkert@gmail.com>
2024-12-30 21:47:45 +0100

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+let random = Math.ceil(Math.random() * 10)
+
+let num3 = 12;
+let num4 = 8;
+let mod = num3 % num4;
+
+let numstrg = (1, 2, 4, 9, 20);
+let max = Math.max(numstrg);
